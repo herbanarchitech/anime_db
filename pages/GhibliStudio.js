1c21ffddda66393093a57043b379c5588770fa7e
herbanarhitech <jeff.badette@protonmail.com>
2021-09-16 11:56:56 -0400

    fetch calls showing and working so

--- pages/GhibliStudio.js
+++ pages/GhibliStudio.js
@@ -14,24 +14,25 @@ DRY: Make one successful then apply it to other pages / studios
 
 This will be used as the bae url */}
 
-//const url='https://ghibliapi.herokuapp.com/films';
- const [ghibliUrl, setURL] = useState([])
-
-
-const getURL= async(url) => {
-
-    try {
+const url='https://ghibliapi.herokuapp.com/films';
+	
+const getURL = async () =>{
+try {
         const response = await axios.get(url);
-        console.log(response); // this would not be ideal to work with since it is returning only as objects ; Rememver .map method works best with data being arrays!
-       //const data = response.data; // if we remove this the browser will diplays the information as verbose a.k.a only viewed as objectes
-       //console.log(data)
+        
+	{/* console logging the repsons is nonsensical => console.log(response).It should only be used to test whether the fetch is actually working. 
+	this would not be ideal to work with since it is returning only as objects ; Rememver .map method works best with data being arrays! For the respose below that we would use. If doesn'nt make sense , comment it out to see the difference. Reason why is not a good idee is, if we need to map over the data , it's impossible since we the map methods works best with array method only*/}
+	
+
+	//console.log(response); // this would not be ideal to work with since it is returning only as objects ; Rememver .map method works best with data being arrays!
+       const data = response.data;
+       console.log(data)
     } catch (err) {
 
         console.log(err)
         
     }   
-
-}
+};
 
 getURL(url);
 return(
